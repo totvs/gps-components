943b5cac446366e3ddbd97d6331e37c472d88dff
Ezequiel Gandolfi <ezequielgandolfi@gmail.com>
2020-03-09 16:50:38 -0300

    componente gps-page-list

--- projects/totvs-gps-controls/src/public_api.ts
+++ projects/totvs-gps-controls/src/public_api.ts
@@ -3,8 +3,13 @@
  */
 
 export * from './lib/gps-export-data/gps-export-data.component';
+
 export * from './lib/gps-order-list/gps-order-list.component';
+
 export * from './lib/gps-rpw/gps-rpw.component';
+
+export * from './lib/gps-page/gps-page-list/gps-page-list.component';
+export * from './lib/gps-page/models/gps-page.model';
 
 export * from './lib/totvs-gps-controls.component';
 export * from './lib/totvs-gps-controls.model';
