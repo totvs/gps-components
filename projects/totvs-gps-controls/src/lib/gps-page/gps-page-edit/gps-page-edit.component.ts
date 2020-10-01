2c25d618bb851e437346610768a09ad6f15f3a59
Ezequiel Gandolfi <ezequielgandolfi@gmail.com>
2020-10-01 08:01:23 -0300

    totvs-gps-controls v4.2.1

--- projects/totvs-gps-controls/src/lib/gps-page/gps-page-edit/gps-page-edit.component.ts
+++ projects/totvs-gps-controls/src/lib/gps-page/gps-page-edit/gps-page-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ViewChild } from "@angular/core";
+import { Component, Input, Output, EventEmitter, ViewChild, OnInit } from "@angular/core";
 import { GpsPageBaseComponent } from "../gps-page-base.component";
 import { ILoadingData, ICustomFields } from "../gps-page.internal-model";
 import { PoPageEditComponent } from "@po-ui/ng-components";
@@ -9,7 +9,7 @@ import { IModelFillData } from "../../totvs-gps-controls.model";
     templateUrl: './gps-page-edit.component.html',
     providers: []
 })
-export class GpsPageEditComponent extends GpsPageBaseComponent {
+export class GpsPageEditComponent extends GpsPageBaseComponent implements OnInit {
 
     //#region Portinari properties
     @ViewChild(PoPageEditComponent, {static:true}) poPageEditComponent: PoPageEditComponent;
@@ -25,6 +25,12 @@ export class GpsPageEditComponent extends GpsPageBaseComponent {
     @Input('model-fill') parameterModelFill: Array<IModelFillData>;
     //#endregion
 
+    //#region startup
+    ngOnInit() {
+        this.setupActions();
+    }
+    //#endregion
+
     //#region loading
     public loadingStatus: ILoadingData = { active: false, message: '' };
     public showLoading(message?: string) {
@@ -34,6 +40,14 @@ export class GpsPageEditComponent extends GpsPageBaseComponent {
     public hideLoading() {
         this.loadingStatus.active = false;
         this.loadingStatus.message = '';
+    }
+    //#endregion
+
+    //#region Page Actions
+    private setupActions() {
+        this.poPageEditComponent.cancel = this.parameterOnCancel;
+        this.poPageEditComponent.save = this.parameterOnSave;
+        this.poPageEditComponent.saveNew = this.parameterOnSaveNew;
     }
     //#endregion
 
