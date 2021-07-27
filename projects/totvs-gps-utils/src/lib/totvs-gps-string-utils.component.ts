fe4fda64200a4fabed76317e7d0064623f41b81b
Kalel Branco Chaves <kalel.chaves@totvs.com.br>
2021-07-27 14:01:58 -0300

    Implementado metodo para formatacao de CPF e CNPJ

--- projects/totvs-gps-utils/src/lib/totvs-gps-string-utils.component.ts
+++ projects/totvs-gps-utils/src/lib/totvs-gps-string-utils.component.ts
@@ -376,4 +376,16 @@ export class GpsStringUtils {
     return "/";
   }
 
+  public static formatTaxpayerNumber(value: string): string {
+    if (value != undefined) {
+        if (value.length == 11) {
+            return value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+        } else if (value.length == 14) {
+            return value.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+        }
+        return value;
+    }
+    return '';
+}
+
 }
